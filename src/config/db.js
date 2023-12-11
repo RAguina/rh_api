c3c16907d95f94ac6dd46e6fbaed41eb51b50aa9
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-11 19:11:24 -0300

    v0.51e

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,23 @@
-import { Sequelize } from 'sequelize';
 
+/*
 const sequelize = new Sequelize('rent_house', 'postgres', 'admin', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432
 });
+*/
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 export default sequelize;
- /* user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: password, 
-  port: process.env.DB_PORT,
-  
-  //ssl: { require:true, rejectUnauthorized: false }
-});
-*/
