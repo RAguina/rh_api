2df45c729d173779b5fd37225b60c66fcf68ecab
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-15 09:49:39 -0300

    v0.54.7

--- src/routes/imagenes_inmuebles.routes.js
+++ src/routes/imagenes_inmuebles.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getImagenesInmueble, uploadImage } from '../controllers/imagenes_inmuebles.controllers.js';
 import { parser } from '../services/uploadImg.js'
+import { authenticateJWT } from '../middleware/jwt.js';
 const router = express.Router();
 
 // Ruta para obtener todas las imágenes de un inmueble
 router.get('/:id', getImagenesInmueble);
 
-router.post('/upload', parser.single('image'), uploadImage);
+router.post('/upload', authenticateJWT, parser.single('image'), uploadImage);
 
 
 export default router;
