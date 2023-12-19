e8367c10e632f74374650ef7490ab99269fe5cc6
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-19 00:15:57 -0300

    v0.57.3

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,19 +1,19 @@
-
+/*
+// Verifica si es una solicitud OPTIONS para la ruta de upload
+if (req.method === 'OPTIONS' && req.url === '/imagen_inmuebles/upload') {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(200).end();
+  console.log('Middleware allowCrossDomain ejecutado 2');
+  return;
+}
+*/
 export const allowCrossDomain = (req, res, next) => {
   console.log('Middleware allowCrossDomain ejecutado');
-  /*
-  // Verifica si es una solicitud OPTIONS para la ruta de upload
-  if (req.method === 'OPTIONS' && req.url === '/imagen_inmuebles/upload') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).end();
-    console.log('Middleware allowCrossDomain ejecutado 2');
-    return;
-  }
-  */
+  
   // Configuración CORS general
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rh-app.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   console.log('Middleware allowCrossDomain ejecutado 2');
--- src/routes/imagenes_inmuebles.routes.js
+++ src/routes/imagenes_inmuebles.routes.js
@@ -4,14 +4,12 @@ import { parser } from '../services/uploadImg.js'
 import { authenticateJWT } from '../middleware/jwt.js';
 const router = express.Router();
 
-
-// Configura CORS para la ruta de upload
-//router.options('/upload', cors());
-
 // Ruta para obtener todas las imágenes de un inmueble
 router.get('/:id', getImagenesInmueble);
 
 router.post('/upload', parser.single('image'), uploadImage);
 
-
 export default router;
+
+// Configura CORS para la ruta de upload
+//router.options('/upload', cors());
