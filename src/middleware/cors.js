b2efb02c41d2b58b21fb6cdc0e1c92102821cf78
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-19 00:27:16 -0300

    v0.57.4

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -16,6 +16,7 @@ export const allowCrossDomain = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://rh-app.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   console.log('Middleware allowCrossDomain ejecutado 2');
   next();
 };
