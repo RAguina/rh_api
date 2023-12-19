470ededf7b0ff7855a74ae2314e077b1be090ede
RAguina <rodrigoaguinagalde@gmail.com>
2023-12-18 23:52:25 -0300

    v0.57.1

--- src/controllers/inmuebles.controllers.js
+++ src/controllers/inmuebles.controllers.js
@@ -70,7 +70,7 @@ export const deleteInmueble = async (req, res) => {
 
 export const createInmueble = async (req, res) => {
   try {
-    const errors = validationResult(req);
+    //const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
